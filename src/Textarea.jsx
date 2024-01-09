81f25f4b5feb6343a2b989a928184c11e3e944ad
Olya <olapancisina1@gmail.com>
2024-01-09 11:26:40 +0100

    add onChange to textarea

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react'
+
 export default function Textarea() {
+
+	const[text, setText] = useState("")
+
 	return (
-		<textarea className='textarea'/>
+		<textarea
+			className='textarea'
+			placeholder='Enter your text'
+			spellCheck='false'
+			value={text}
+			onChange={(e) => {
+				setText(e.target.value)
+			}}
+		/>
 	)
 }

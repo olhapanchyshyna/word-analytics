54ab50450962a230e02139b1687ff9ff39bc0d7b
Olya <olapancisina1@gmail.com>
2024-01-10 12:58:35 +0100

    refactoring stat

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,20 +1,27 @@
-import Stats from './Stats'
 import { useState } from 'react'
+import Stats from './Stats'
 import Textarea from './Textarea'
 
 export default function Container() {
-
 	const [text, setText] = useState('')
-	
-	let numberOfCharasters = text.length
-	let numbeOfWods = text.split(/\s/).filter((word) => word !== "").length
-	let instagramCharactersleft = 280 - text.length;
-	let facebookCharactersleft = 2200 - text.length;
+
+	const numberOfCharasters = text.length
+
+	const stat = {
+		numberOfCharasters: numberOfCharasters,
+		numbeOfWods: text.split(/\s/).filter(word => word !== '').length,
+		instagramCharactersleft: 280 - text.length,
+		facebookCharactersleft: 2200 - text.length,
+	}
 
 	return (
 		<section className='container'>
-				<Textarea text={text} setText={setText} numberOfCharasters={numberOfCharasters}/>
-				<Stats numberOfCharasters={numberOfCharasters} instagramCharactersleft={instagramCharactersleft} facebookCharactersleft={facebookCharactersleft} numbeOfWods={numbeOfWods}/>
+			<Textarea
+				text={text}
+				setText={setText}
+				numberOfCharasters={numberOfCharasters}
+			/>
+			<Stats stat={stat} />
 		</section>
 	)
 }
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,10 +1,10 @@
-export default function Stats({numberOfCharasters, instagramCharactersleft, facebookCharactersleft, numbeOfWods}) {
+export default function Stats({stat}) {
 	return (
 		<section className='stats'>
-			<Stat number={numberOfCharasters} title='Charasters'/>
-			<Stat number={numbeOfWods} title='Words' />
-			<Stat number={instagramCharactersleft > 0 ? instagramCharactersleft : 0 } title='Instagram' />
-			<Stat number={facebookCharactersleft > 0 ? facebookCharactersleft : 0 } title='Facebook' />
+			<Stat number={stat.numberOfCharasters} title='Charasters'/>
+			<Stat number={stat.numbeOfWods} title='Words' />
+			<Stat number={stat.instagramCharactersleft > 0 ? stat.instagramCharactersleft : 0 } title='Instagram' />
+			<Stat number={stat.facebookCharactersleft > 0 ? stat.facebookCharactersleft : 0 } title='Facebook' />
 		</section>
 	)
 }
